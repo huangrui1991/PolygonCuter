05c355bc05d99ad661603fd81e19818e7f05e48b
huangrui <huangrui1991@outlook.com>
2015-08-31 10:52:14 +0800

    init

---
+++ PolygonCuter/PolygonCuter/PolygonCuter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace PolygonCuter
+{
+    public class PolygonCuter : ESRI.ArcGIS.Desktop.AddIns.Tool
+    {
+        public PolygonCuter()
+        {
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
