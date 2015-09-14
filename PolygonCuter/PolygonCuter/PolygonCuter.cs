f6d7ecf85e3cc4238441b77534528f4b64f49501
huangrui <huangrui1991@outlook.com>
2015-09-14 12:42:00 +0800

    linefeedback

--- PolygonCuter/PolygonCuter/PolygonCuter.cs
+++ PolygonCuter/PolygonCuter/PolygonCuter.cs
@@ -2,19 +2,69 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using ESRI.ArcGIS.Geometry;
+
+using ESRI.ArcGIS.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geometry;
 
 namespace PolygonCuter
 {
     public class PolygonCuter : ESRI.ArcGIS.Desktop.AddIns.Tool
     {
+
+        private bool m_isMouseDown = false;
+        private INewLineFeedback m_lineFeedback = null;
+        private IPoint m_currentPoint = null;
+
+
         public PolygonCuter()
         {
+        }
+
+        protected override void OnActivate()
+        {
+            Stream sm = this.GetType().Assembly.GetManifestResourceStream("PolygonCuter.Images.Sketch.cur");
+            this.Cursor = new System.Windows.Forms.Cursor(sm);
+            
         }
 
         protected override void OnUpdate()
         {
             Enabled = ArcMap.Application != null;
         }
+
+        protected override void OnMouseDown(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
+        {
+            if (m_lineFeedback == null)
+            {
+                m_lineFeedback = new NewLineFeedback();
+                m_lineFeedback.Display = ArcMap.Document.ActiveView.ScreenDisplay;
+            }
+            if (m_isMouseDown == false)
+            {
+                m_isMouseDown = true;
+                m_lineFeedback.Start(m_currentPoint);
+            }
+            else
+            {
+                m_lineFeedback.AddPoint(m_currentPoint);
+            }
+        }
+
+        protected override void OnMouseMove(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
+        {
+            m_currentPoint = ArcMap.Document.ActiveView.ScreenDisplay.DisplayTransformation.ToMapPoint(arg.X, arg.Y);
+            if (m_lineFeedback == null)
+                return;
+            m_lineFeedback.MoveTo(m_currentPoint);
+        }
+
     }
 
 }
